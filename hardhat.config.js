abb371a07f6a3b9c411526b765cee49e95726730
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-27 15:08:57 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
